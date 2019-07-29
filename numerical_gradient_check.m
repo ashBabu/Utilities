6d0a51c6d56409a0d4e7ac33c686f7cde3ab7c98
Ash Babu <shyamashi@users.noreply.github.com>
2019-07-29 22:51:45 +0100

    Create numerical_gradient_check.m

---
+++ numerical_gradient_check.m
@@ -0,0 +1,8 @@
+epsilon = 1e-4;
+for i = 1:n, # n = number of parameters
+  thetaPlus = theta;
+  thetaPlus(i) += epsilon;
+  thetaMinus = theta;
+  thetaMinus(i) -= epsilon;
+  gradApprox(i) = (J(thetaPlus) - J(thetaMinus))/(2*epsilon)
+end;
