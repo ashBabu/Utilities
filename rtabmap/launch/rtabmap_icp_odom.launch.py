21109b092cdb80cde8935887cc1f6d4d60e692f8
ashbabu <shyamashi@gmail.com>
2025-03-19 14:36:54 +0000

    updates
    Signed-off-by: ashbabu <shyamashi@gmail.com>

--- rtabmap/launch/rtabmap_icp_odom.launch.py
+++ rtabmap/launch/rtabmap_icp_odom.launch.py
@@ -7,9 +7,9 @@ def generate_launch_description():
 
     ld = LaunchDescription()
 
-    arg_scan_topic = LaunchConfiguration('lidar_scan_topic')
-    arg_odom_topic = LaunchConfiguration('lidar_odom_topic')
-    arg_base_frame = LaunchConfiguration('base_frame')
+    arg_scan_topic = LaunchConfiguration('lidar_scan_topic', default='/okdo/scan')
+    arg_odom_topic = LaunchConfiguration('lidar_odom_topic', default='/okdo/odom')
+    arg_base_frame = LaunchConfiguration('base_frame', default='base_link')
     arg_need_tf = LaunchConfiguration('need_tf', default=True)
     arg_sim = LaunchConfiguration('sim', default=False)
 
@@ -18,39 +18,42 @@ def generate_launch_description():
         # "frame_id": "base_laser",  # Robot base frame
         "odom_frame_id": "odom",  # Odometry frame
         "publish_tf": arg_need_tf,  # robot_localization would publish this
-        "approx_sync": True,
-        # "wait_for_transform": True,
-        'subscribe_scan': True,
+        "approx_sync": "True",
+        # "wait_imu_to_init": "True",
+        # "wait_for_transform": "True",
+        'subscribe_scan': "True",
         "use_sim_time": arg_sim,
 
         # ICP parameters
         # Enable Point-to-Plane ICP (better for structured environments)
-        "icp/pointToPlane": True,
-        "icp/iterations": 30,      # Number of ICP iterations per scan
-        "icp/maxCorrespondenceDistance": 0.2,  # Max distance for point matching
-        "icp/downsamplingStep": 2,  # Reduce point cloud density for efficiency
+        "icp/PointToPlane": "True",
+        "icp/Strategy": 0,
+        "icp/iterations": "30",      # Number of ICP iterations per scan
+        "icp/maxCorrespondenceDistance": "0.2",  # Max distance for point matching
+        "icp/downsamplingStep": "2",  # Reduce point cloud density for efficiency
         # Ratio of matched points to accept a transformation
-        "icp/correspondenceRatio": 0.3,
-        "icp/voxelSize": 0.05,  # Set voxel size for ICP (meters)
+        "icp/correspondenceRatio": "0.3",
+        "icp/voxelSize": "0.05",  # Set voxel size for ICP (meters)
 
         # Odometry filtering & robustness
-        "reg/strategy": 1,  # Use ICP instead of visual features
+        "reg/strategy": "1",  # Use ICP instead of visual features
         # Keyframe threshold (higher = fewer keyframes)
-        "odom/scanKeyFrameThr": 0.6,
-        "odom/guessMotion": True,  # Use constant velocity model for prediction
+        "odom/scanKeyFrameThr": "0.6",
+        "odom/guessMotion": "True",  # Use constant velocity model for prediction
         "Odom/Strategy": "0",
         # "Vis/CorType": "1",  # 0=Features Matching, 1=Optical Flow
         "OdomF2M/MaxSize": "1000",  # maximum features map size, default 2000
         "Odom/ResetCountdown": "1",
         # https://answers.ros.org/question/250528/rtabmap-how-to-save-registered-3d-point-cloud/
-        "Grid/FromDepth": "false",
-        "Reg/Force3DoF": "true",          # for 2D
+        "Grid/FromDepth": "False",
+        "Reg/Force3DoF": "True",          # for 2D
         # "Optimizer/Slam2D": "true",        # for 2D
     }]
 
     remappings = [
         ("/scan", arg_scan_topic),  
-        ('odom', arg_odom_topic)  
+        ('odom', arg_odom_topic), 
+        ('imu', '/camera/imu/data')  
     ]
 
     node_rtabmap_icp_odom = Node(
--- rtabmap/launch/rtabmap_slam.launch.py
+++ rtabmap/launch/rtabmap_slam.launch.py
@@ -33,13 +33,29 @@ def generate_launch_description():
         {'database_path': database_path},
         {'subscribe_depth': True},
         {'subscribe_scan': True},
-        {'subscribe_odom_info': True},
-        {'approx_sync': True},
+        # {'subscribe_odom_info': True},
+        # {'approx_sync': True},
         {"frame_id": arg_base_frame},
-        {"odom_frame_id": "odom"},
-        {"queue_size": 50},
-        {"qos": 2},
-        {"qos_imu": 2},
+        # {"odom_frame_id": "odom"},
+        # {"queue_size": 50},
+        # {"qos": 2},
+        # {"qos_imu": 2},
+        # {"qos_imu": 2},
+        {"Reg/Strategy": "1"},                         # "0=Vis, 1=Icp, 2=VisIcp"
+        {"Reg/Force3DoF": "True"},                    
+        # {'Rtabmap/DetectionRate': "1"},              # Detection rate (Hz). RTAB-Map will filter input images to satisfy this rate.
+        # {'Rtabmap/ImagesAlreadyRectified': "False"},   # By default RTAB-Map assumes that received images are rectified. If they are not, they can be rectified by RTAB-Map if this parameter is false.
+        # {'Rtabmap/LoopThr': "0.11"},                   # Loop closing threshold.
+        {'Odom/Strategy': "0"},                        # [0=Frame-to-Map (F2M) 1=Frame-to-Frame (F2F) 2=Fovis 3=viso2 4=DVO-SLAM 5=ORB_SLAM2 6=OKVIS 7=LOAM 8=MSCKF_VIO 9=VINS-Fusion 10=OpenVINS 11=FLOAM 12=Open3D]
+        {'Odom/FilteringStrategy': 2},               # [0=No filtering 1=Kalman filtering 2=Particle filtering.]
+        # {'Optimizer/Strategy': "1"},                   # Graph optimization strategy: 0=TORO, 1=g2o, 2=GTSAM and 3=Ceres. Only 1 or 2 can be used.
+        # {'Optimizer/Robust': True},                  # Robust graph optimization using Vertigo (only work for g2o and GTSAM optimization strategies). Not compatible with {'RGBD/OptimizeMaxError' if enabled.
+        # {'Optimizer/GravitySigma': 0.3},
+        # {'RGBD/SavedLocalizationIgnored': False},    # Ignore saved localization if the loop closure is detected. This will force RTAB-Map to localize again in the current map.
+        # {'Mem/IncrementalMemory': False},            # true = mapping mode
+        # {'Mem/ReduceGraph': True},                   # Shuld make the database smaller.
+        # {'Mem/SaveDepth16Format': True},             # Save depth images in 16 bit format to save size.
+        # {'Mem/UseOdomGravity': True},                # Use odometry instead of IMU orientation to add gravity links to new nodes created.
         # {"publish_tf": True},
     ]
 
@@ -49,7 +65,8 @@ def generate_launch_description():
         ('rgb/camera_info', arg_rgb_cam_info_topic),
         ('depth/image', arg_depth_img_topic),
         ('scan', arg_lidar_scan_topic),
-        ('odom', '/odometry/filtered'),
+        # ('odom', '/odometry/filtered'),
+        ('odom', '/okdo/odom'),
     ]
 
     node_rtabmap_slam = Node(
